2df0f02c0f2d1b9ebd1889e50f783eea8444e644
Joseph Romano <JDRomano2@gmail.com>
2018-02-23 16:27:37 -0500

    Clean up MultiGexpro.cpp

--- gexpro/multiGexpro.cpp
+++ gexpro/multiGexpro.cpp
@@ -7,34 +7,6 @@
 #include <memory>
 #include <cstdlib>
 
-// // For debugging (see https://stackoverflow.com/a/20170989/1730417)
-// template <class T>
-// std::string
-// type_name()
-// {
-//   typedef typename std::remove_reference<T>::type TR;
-//   std::unique_ptr<char, void(*)(void*)> own
-//     (
-// #ifndef _MSC_VER
-//      abi::__cxa_demangle(typeid(TR).name(), nullptr,
-//                          nullptr, nullptr),
-// #else
-//      nullptr,
-// #endif
-//      std::free
-//      );
-//   std::string r = own != nullptr ? own.get() : typeid(TR).name();
-//   if (std::is_const<TR>::value)
-//     r += " const";
-//   if (std::is_volatile<TR>::value)
-//     r += " volatile";
-//   if (std::is_lvalue_reference<T>::value)
-//     r += "&";
-//   else if (std::is_rvalue_reference<T>::value)
-//     r += "&&";
-//   return r;
-// }
-
 MultiGexpro::MultiGexpro(Gexpro& firstGexpro) {
   // Unfortunately we can't make a vector of references; store instead as vector of pointers
   // (perhaps reimplement as boost::ptr_vector)
@@ -113,9 +85,8 @@ void MultiGexpro::findCommonFeatures() {
   int num_obs_gexpros = 0;
   for (auto k: gps) {
     num_obs_gexpros++;
-    for (auto l: k->getFeatureNames()) {
+    for (auto l: k->getFeatureNames())
       (feat_map[l])++;
-    }
   }
 
   std::vector<std::string> in_all;
@@ -136,20 +107,7 @@ void MultiGexpro::findCommonFeatures() {
 }
 
 void MultiGexpro::removeNonCommonFeatures() {
-  // replace the following with (VERBOSE)
-  // if (true) {
-  //   std::cout << "==============================================" << std::endl;
-  //   std::cout << "REMOVING NON-COMMON FEATURES" << std::endl;
-  //   std::cout << std::endl;
-  //   std::cout << "Number of common features: " << common_features.size() << std::endl;
-  //   std::cout << "  Number of features in gp1: " << gps[0]->getNFeatures() << std::endl;
-  //   std::cout << "  Number of samples in gp1: " << gps[0]->getNSamples() << std::endl;
-  //   std::cout << std::endl;
-  //   std::cout << "  Number of features in gp2: " << gps[1]->getNFeatures() << std::endl;
-  //   std::cout << "  Number of samples in gp2: " << gps[1]->getNSamples() << std::endl;
-  //   std::cout << std::endl;
-  // }
-
+  // May be able to make this more efficient
   for (auto i: uncommon_features)
     for (auto j: gps)
       j->removeFeatureByName(i, true);
@@ -161,8 +119,7 @@ void MultiGexpro::imputeZeros() {
 
 void MultiGexpro::normalizeAllGexpros() {
   std::cout << "Normalizing all data..." << std::endl;
-  for (auto i: gps) {
+  for (auto i: gps)
     i->normalizeFromDataMatrix();
-  }
   std::cout << "...successfully normalized all datasets." << std::endl;
 }
